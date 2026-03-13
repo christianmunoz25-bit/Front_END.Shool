d1e5e8d4c37ab250a29ce56df4217cbaccbcaf9d
Christian Panilago Muñoz <christianixxx2004@gmail.com>
2026-03-13 14:03:46 +0800

    Update index-1.js

--- index-1.js
+++ index-1.js
@@ -1,1 +1,27 @@
+document.getElementById('name').textContent = 'Christian Muñoz';
+document.getElementById('tagline').textContent = "Aspiring Software | Full-Stack Developer";
+
+
+document.getElementById('age').textContent = "21";
+document.getElementById('hometown').textContent = "Sto. Niño St., Barangay Taytay, Danao City 6004, Cebu, Philippines";
+document.getElementById('course').textContent = "ACT - Software Development";
+document.getElementById('year-level').textContent = "1st Year";
+
+document.getElementById('bio').textContent = "I'm currently a student which a strong specialization of Web Development";
+
+document.getElementById('interest-1').textContent = "Freelancing";
+document.getElementById('interest-2').textContent = "User Interface/User Experience";
+document.getElementById('interest-3').textContent = "Open Source";
+
+
+document.getElementById('email').textContent = "chanixxx2004@gmail.com";
+document.getElementById('github').textContent = "https://github.com/christianmunoz25-bit";
+document.getElementById('location').textContent = "Pahina Central (Pob.), Colon St., Cebu City";
+
+
+
+
+
+
+
 
