aa2a071fab487202a68a7f12b302a5e8251dcb21
Christian Panilago Muñoz <christianixxx2004@gmail.com>
2026-03-15 18:59:11 -0700

    Update index-1.js

--- index-1.js
+++ index-1.js
@@ -1,23 +1,21 @@
-document.getElementById('name').textContent = 'Christian Muñoz';
-document.getElementById('tagline').textContent = "Aspiring Software | Full-Stack Developer";
+document.getElementById('name').textContent = 'Christian P. Muñoz';
+document.getElementById('tagline').textContent = "Aspiring Software/Full-Stack Developer";
 
 
-document.getElementById('age').textContent = "21";
+document.getElementById('age').textContent = "21 years-old";
 document.getElementById('hometown').textContent = "Sto. Niño St., Barangay Taytay, Danao City 6004, Cebu, Philippines";
-document.getElementById('course').textContent = "ACT - Software Development";
+document.getElementById('course').textContent = "ACT-Software Development";
 document.getElementById('year-level').textContent = "1st Year";
 
-document.getElementById('bio').textContent = "I'm currently a student which a strong specialization of Web Development";
+document.getElementById('bio').textContent = "I'm currently a student learning specialization in Web and Software Development.";
 
-document.getElementById('interest-1').textContent = "Freelancing";
-document.getElementById('interest-2').textContent = "User Interface/User Experience";
-document.getElementById('interest-3').textContent = "Open Source";
-
+document.getElementById('interest-1').textContent = "Building small Personal Projects";
+document.getElementById('interest-2').textContent = "I am interested in UI/UX design.";
+document.getElementById('interest-3').textContent = "Learning Front-end (HTML,CSS,Javascript)";
 
 document.getElementById('email').textContent = "chanixxx2004@gmail.com";
 document.getElementById('github').textContent = "https://github.com/christianmunoz25-bit";
-document.getElementById('location').textContent = "Pahina Central (Pob.), Colon St., Cebu City";
-
+document.getElementById('location').textContent = "Pahina Central, Colon St., Cebu City";
 
 
 
